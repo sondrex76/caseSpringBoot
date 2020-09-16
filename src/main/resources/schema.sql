09a734b53c0eef02a8d934be0d42bc944df63b22
Sondre Ropstad <sonwrop@gmail.com>
2020-09-16 14:28:42 +0200

    Added starting point for project

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+CREATE TABLE employee
+
+(
+
+ characterName varchar(100) NOT NULL,
+
+  characterId varchar(11) NOT NULL ,
+
+ characterExperienceValue varchar(100) DEFAULT NULL,
+
+ characterDetails varchar(1000) DEFAULT NULL,
+
+ characterPicture varchar(1000) DEFAULT NULL,
+
+ PRIMARY KEY (characterId)
+
+);
