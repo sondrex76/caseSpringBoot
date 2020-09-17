e6f54d15f4bfb49eb21b397d331a1655a381a12d
Sondre Ropstad <sonwrop@gmail.com>
2020-09-17 08:16:52 +0200

    Fixed some errors in characters.html

--- src/main/java/com/example/demo/CharactersController.java
+++ src/main/java/com/example/demo/CharactersController.java
@@ -16,8 +16,8 @@ public class CharactersController {
 	public String characters(Model model) {
 		// TODO: Extract data from database
 		// TODO: Send data to characters.html as an array
-		
 		// TODO: Make characters.html display all objects in array
+		
 		// TODO: Make characters.html display character objects clickable and lead to openCharacter.html
 		// TODO: Make button leading to template form for adding new object to database
 		// TODO: Allow image to be shown when character is open
